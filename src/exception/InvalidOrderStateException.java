a9cc5517f906730f307749ee3ce9a9d4c92e2d40
Hungdo2005 <quanghung2005thd@gmail.com>
2026-07-21 08:55:18 +0700

    feat(member1): add InvalidOrderStateException.java

---
+++ src/exception/InvalidOrderStateException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class InvalidOrderStateException extends InvalidOrderException {
+    public InvalidOrderStateException(String message) {
+        super(message);
+    }
+}
